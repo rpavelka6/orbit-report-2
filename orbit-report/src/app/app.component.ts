aac1c519aac15ca8cf479eb09492707a105fd1ee
Rebecca Pavelka <pavelka6@sbcglobal.net>
2019-10-15 22:46:59 -0500

    It works after step 4! The table displays!

--- orbit-report/src/app/app.component.ts
+++ orbit-report/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Satellite } from './satellite';
+
+let sourceList: Satellite[];
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  sourceList: Satellite[];
   title = 'orbit-report';
+  constructor() {
+    this.sourceList = [
+      new Satellite("SiriusXM", "Communication", "2009-03-21", "LOW", true),
+      new Satellite("Cat Scanner", "Imaging", "2012-01-05", "LOW", true),
+      new Satellite("Weber Grill", "Space Debris", "1996-03-25", "HIGH", false),
+      new Satellite("GPS 938", "Positioning", "2001-11-01", "HIGH", true),
+      new Satellite("ISS", "Space Station", "1998-11-20", "LOW", true),
+   ];
+  }
 }
