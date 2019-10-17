2142805326e4c5a80e474e3bb26a32bcaceaf28d
Rebecca Pavelka <pavelka6@sbcglobal.net>
2019-10-16 20:25:54 -0500

    Step 8 - Search works

--- orbit-report/src/app/app.component.ts
+++ orbit-report/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Satellite } from './satellite';
 
 let sourceList: Satellite[];
+let displayList: Satellite[];
 
 @Component({
   selector: 'app-root',
@@ -26,9 +27,23 @@ export class AppComponent {
           for (let i=0; i<fetchedSatellites.length; i++){
             let satellite = new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
             this.sourceList.push(satellite);
-            this.displayList.slice(0);
-          }
+            this.displayList = this.sourceList.slice(0);
+          }  
        }.bind(this));
     }.bind(this));
  }
+
+ search(searchTerm: string): void {
+  let matchingSatellites: Satellite[] = [];
+  searchTerm = searchTerm.toLowerCase();
+  for(let i=0; i < this.sourceList.length; i++) {
+     let name = this.sourceList[i].name.toLowerCase();
+     if (name.indexOf(searchTerm) >= 0) {
+        matchingSatellites.push(this.sourceList[i]);
+     }
+  }
+  // assign this.displayList to be the the array of matching satellites
+  // this will cause Angular to re-make the table, but now only containing matches
+  this.displayList = matchingSatellites;
+}
 }
